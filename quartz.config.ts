b7f77e78f2411e563fd102a6320d2fa100999326
Roxrad-Dev <roxrad.dev@gmail.com>
2026-01-24 20:19:09 +0100

    Quartz sync: Jan 24, 2026, 8:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Rogue Shmup project",
+    pageTitle: "Project Paradox",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
