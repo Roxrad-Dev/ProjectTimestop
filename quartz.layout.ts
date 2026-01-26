c3fc263a6a438beff9a8137cc83e78f55139f157
Roxrad-Dev <roxrad.dev@gmail.com>
2026-01-26 21:21:09 +0100

    Quartz sync: Jan 26, 2026, 9:21 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      "Comming soon": "https://roxrad-dev.github.io/shmuproguelike/",
+      "Comming soon": "https://roxrad-dev.github.io/ProjectTimestop/",
     },
   }),
 }
